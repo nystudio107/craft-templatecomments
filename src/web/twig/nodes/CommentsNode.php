df46f755c10c08282443e751a4f09e1ff6b636e2
Andrew Welch <andrew@nystudio107.com>
2018-10-04 10:46:19 -0400

    Fix comment blocks
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/web/twig/nodes/CommentsNode.php
+++ src/web/twig/nodes/CommentsNode.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Template Comments plugin for Craft CMS 3.x
+ *
+ * Adds a HTML comment to demarcate each Twig template that is included or extended.
+ *
+ * @link      https://nystudio107.com/
+ * @copyright Copyright (c) 2018 nystudio107
+ */
 
 namespace nystudio107\templatecomments\web\twig\nodes;
 
+/**
+ * @author    nystudio107
+ * @package   TemplateComments
+ * @since     1.0.0
+ */
 class CommentsNode extends \Twig_Node
 {
     // Public Methods
