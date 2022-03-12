afedeebbc9a5205127158803f8e248c76853118a
Andrew Welch <andrew@nystudio107.com>
2022-03-11 22:47:34 -0500

    refactor: Use VitePress for documentation

---
+++ docs/docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'Template Comments Plugin Documentation',
+  description: 'Documentation for the Template Comments plugin',
+  base: '/docs/template-comments/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://twitter.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://youtube.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    repo: 'nystudio107/craft-templatecomments',
+    docsDir: 'docs/docs',
+    docsBranch: 'v1',
+    algolia: {
+      appId: '',
+      apiKey: '',
+      indexName: ''
+    },
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    sidebar: 'auto',
+  },
+};
